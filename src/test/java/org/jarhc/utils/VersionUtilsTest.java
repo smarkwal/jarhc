1ca8bef79bb07c03329d6a4c83b10908c2439fc4
Stephan Markwalder <stephan.markwalder@appway.com>
2019-04-22 16:49:09 +0200

    Version 1.3-SNAPSHOT

--- src/test/java/org/jarhc/utils/VersionUtilsTest.java
+++ src/test/java/org/jarhc/utils/VersionUtilsTest.java
@@ -32,7 +32,7 @@ class VersionUtilsTest {
 		// assert
 		assertNotNull(version);
 		assumeFalse(version.equals("${project.version}"), () -> "jarhc.properties not filtered.");
-		assertEquals("1.2", version);
+		assertEquals("1.3-SNAPSHOT", version);
 
 	}
 
