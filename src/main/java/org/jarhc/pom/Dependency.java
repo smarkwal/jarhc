e8aafc8d72da43cbab2240af73901ee2cf035d6c
Stephan Markwalder <stephan.markwalder@appway.com>
2019-09-28 22:06:36 +0200

    Code: Implement parser for POM XML files.

---
+++ src/main/java/org/jarhc/pom/Dependency.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2019 Stephan Markwalder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jarhc.pom;
+
+public class Dependency {
+
+	private final String groupId;
+	private final String artifactId;
+	private final String version;
+	private final Scope scope;
+	private final boolean optional;
+
+	Dependency(String groupId, String artifactId, String version, Scope scope, boolean optional) {
+		if (groupId == null || groupId.isEmpty()) throw new IllegalArgumentException("groupId");
+		if (artifactId == null || artifactId.isEmpty()) throw new IllegalArgumentException("artifactId");
+		if (version == null) throw new IllegalArgumentException("version");
+		this.groupId = groupId;
+		this.artifactId = artifactId;
+		this.version = version;
+		this.scope = scope;
+		this.optional = optional;
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public Scope getScope() {
+		return scope;
+	}
+
+	public boolean isOptional() {
+		return optional;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Dependency dependency = (Dependency) obj;
+		if (!groupId.equals(dependency.groupId)) return false;
+		if (!artifactId.equals(dependency.artifactId)) return false;
+		if (!version.equals(dependency.version)) return false;
+		if (scope != dependency.scope) return false;
+		return optional == dependency.optional;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder buffer = new StringBuilder();
+		buffer.append(groupId).append(":");
+		buffer.append(artifactId).append(":");
+		buffer.append(version).append(" (");
+		buffer.append(scope.name().toLowerCase());
+		if (optional) {
+			buffer.append(", optional");
+		}
+		buffer.append(")");
+		return buffer.toString();
+	}
+
+}
---
+++ src/test/java/org/jarhc/pom/DependencyTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Stephan Markwalder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jarhc.pom;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class DependencyTest {
+
+	@Test
+	void test_JUnit() {
+
+		Dependency dependency = new Dependency("junit", "junit", "4.12", Scope.TEST, false);
+
+		assertEquals("junit", dependency.getGroupId());
+		assertEquals("junit", dependency.getArtifactId());
+		assertEquals("4.12", dependency.getVersion());
+		assertEquals(Scope.TEST, dependency.getScope());
+		assertFalse(dependency.isOptional());
+
+		assertEquals("junit:junit:4.12 (test)", dependency.toString());
+
+	}
+
+	@Test
+	void test_ASM() {
+
+		Dependency dependency = new Dependency("org.ow2.asm", "asm", "7.1", Scope.COMPILE, true);
+
+		assertEquals("org.ow2.asm", dependency.getGroupId());
+		assertEquals("asm", dependency.getArtifactId());
+		assertEquals("7.1", dependency.getVersion());
+		assertEquals(Scope.COMPILE, dependency.getScope());
+		assertTrue(dependency.isOptional());
+
+		assertEquals("org.ow2.asm:asm:7.1 (compile, optional)", dependency.toString());
+
+	}
+
+}
