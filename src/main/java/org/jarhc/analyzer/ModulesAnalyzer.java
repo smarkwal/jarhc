f9e4370866484049759fe919de32ae689bbea9cc
Stephan Markwalder <stephan.markwalder@appway.com>
2021-12-27 22:09:59 +0100

    Code: Fix copyright file header.

--- src/main/java/org/jarhc/analyzer/ModulesAnalyzer.java
+++ src/main/java/org/jarhc/analyzer/ModulesAnalyzer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Stephan Markwalder
+ * Copyright 2021 Stephan Markwalder
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
