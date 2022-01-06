3de03e7693bf071349ba52c52449963ee49eccb7
smarkwal <stephan@markwalder.net>
2022-01-06 20:09:45 +0100

    Tests: Fix release tests. Delete post-build validation tests.

--- src/test/sh/app/Version.java
+++
@@ -1,8 +0,0 @@
-public class Version {
-	public static void main(String[] args) {
-		String version = System.getProperty("java.version");
-		String vendor = System.getProperty("java.vendor");
-		String home = System.getProperty("java.home");
-		System.out.println(version + " | " + vendor + " | " + home);
-	}
-}
