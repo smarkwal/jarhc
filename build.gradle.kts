eb70310e9556e6862d9e666dc92bc37a514b6710
smarkwal <stephan@markwalder.net>
2022-01-10 11:52:05 +0100

    Project: Upgrade jcl-over-slf4j to version 1.7.32 to be aligned with version of other SLF4J libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -182,6 +182,7 @@ dependencies {
     implementation("org.apache.maven:maven-aether-provider:3.3.9")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("org.slf4j:jul-to-slf4j:1.7.32")
+    implementation("org.slf4j:jcl-over-slf4j:1.7.32")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.32")
 
     // fix vulnerabilities in transitive dependencies
