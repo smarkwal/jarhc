73391e3bbeab29853e34ab9f7cac71fa5d5b52ef
smarkwal <stephan@markwalder.net>
2022-11-28 13:27:16 +0100

    Bump SLF4J from 2.0.4 to 2.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -241,8 +241,8 @@ dependencies {
     releaseTestImplementation("org.testcontainers:junit-jupiter:1.17.6")
     releaseTestImplementation("org.apache.commons:commons-lang3:3.12.0")
     releaseTestImplementation("commons-io:commons-io:2.11.0")
-    releaseTestRuntimeOnly("org.slf4j:slf4j-api:2.0.4")
-    releaseTestRuntimeOnly("org.slf4j:slf4j-simple:2.0.4")
+    releaseTestRuntimeOnly("org.slf4j:slf4j-api:2.0.5")
+    releaseTestRuntimeOnly("org.slf4j:slf4j-simple:2.0.5")
 
 }
 
