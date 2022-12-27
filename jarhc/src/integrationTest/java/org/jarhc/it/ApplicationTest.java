f6610daa1a1712bfba05e8f9b31c5f74e2392baf
smarkwal <stephan@markwalder.net>
2022-12-27 14:45:51 +0100

    Tests: Add more integration tests for new code.

--- jarhc/src/integrationTest/java/org/jarhc/it/ApplicationTest.java
+++ jarhc/src/integrationTest/java/org/jarhc/it/ApplicationTest.java
@@ -32,27 +32,36 @@ import org.jarhc.test.JavaRuntimeMock;
 import org.jarhc.test.PrintStreamBuffer;
 import org.jarhc.test.TextUtils;
 import org.jarhc.test.log.LoggerBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
 
 class ApplicationTest {
 
-	@Test
-	void test(@TempDir Path tempDir) throws IOException {
+	PrintStreamBuffer out = new PrintStreamBuffer();
+	Logger applicationLogger = LoggerBuilder.collect(Application.class);
+	Logger mavenRepositoryLogger = LoggerBuilder.collect(MavenRepository.class);
 
-		// prepare
-		PrintStreamBuffer out = new PrintStreamBuffer();
+	Application application = new Application(applicationLogger);
 
-		Logger applicationLogger = LoggerBuilder.collect(Application.class);
-		Application application = new Application(applicationLogger);
+	@TempDir
+	Path tempDir;
+
+	@BeforeEach
+	void setUp() {
 		application.setOut(out);
 		application.setJavaRuntimeFactory(JavaRuntimeMock::getOracleRuntime);
-		Logger mavenRepositoryLogger = LoggerBuilder.collect(MavenRepository.class);
+
 		ArtifactFinder artifactFinder = ArtifactFinderMock.getArtifactFinder();
 		MavenRepository repository = new MavenRepository(11, TestUtils.getFileRepositoryURL(), tempDir.toString(), artifactFinder, mavenRepositoryLogger);
 		application.setRepository(repository);
+	}
 
+	@Test
+	void test() throws IOException {
+
+		// prepare
 		Options options = new Options();
 		File file = TestUtils.getResourceAsFile("/org/jarhc/it/ApplicationTest/a.jar", tempDir);
 		options.addClasspathJarPath(file.getAbsolutePath());
@@ -84,6 +93,55 @@ class ApplicationTest {
 
 		assertLogger(mavenRepositoryLogger)
 				.hasDebug("Find artifact: b2de6f7c6eff51a28729be9c4f6555354f16a1ca")
+				.isEmpty();
+
+	}
+
+	@Test
+	void test_options() throws IOException {
+
+		// prepare
+		Options options = new Options();
+		options.addClasspathJarPath("org.ow2.asm:asm-tree:7.0");
+		options.addClasspathJarPath("org.ow2.asm:asm-commons:7.0");
+		options.addProvidedJarPath("org.ow2.asm:asm:7.0");
+		options.setSkipEmpty(true);
+		options.setSortRows(true);
+
+		// test
+		int exitCode = application.run(options);
+
+		// assert
+		assertEquals(0, exitCode);
+
+		String output = out.getText();
+
+		if (TestUtils.createResources()) {
+			TestUtils.saveResource("integrationTest", "/org/jarhc/it/ApplicationTest/result_options.txt", output, "UTF-8");
+			return;
+		}
+
+		String expectedOutput = TestUtils.getResourceAsString("/org/jarhc/it/ApplicationTest/result_options.txt", "UTF-8");
+
+		// normalize output
+		output = TextUtils.toUnixLineSeparators(output);
+		expectedOutput = TextUtils.toUnixLineSeparators(expectedOutput);
+
+		assertEquals(expectedOutput, output);
+
+		assertLogger(applicationLogger)
+				.hasDebug("Time: *")
+				.isEmpty();
+
+		assertLogger(mavenRepositoryLogger)
+				.hasDebug("Download artifact: org.ow2.asm:asm:7.0:jar")
+				.hasDebug("Find artifact: d74d4ba0dee443f68fb2dcb7fcdb945a2cd89912")
+				.hasDebug("Download artifact: org.ow2.asm:asm-commons:7.0:jar")
+				.hasDebug("Download artifact: org.ow2.asm:asm-tree:7.0:jar")
+				.hasDebug("Find artifact: 29bc62dcb85573af6e62e5b2d735ef65966c4180")
+				.hasDebug("Find artifact: 478006d07b7c561ae3a92ddc1829bca81ae0cdd1")
+				.hasDebug("Get dependencies: org.ow2.asm:asm-commons:7.0:jar")
+				.hasDebug("Get dependencies: org.ow2.asm:asm-tree:7.0:jar")
 				.isEmpty();
 
 	}
