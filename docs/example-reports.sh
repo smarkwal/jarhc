d1cd1ab4c550a922b0a590419430f98ce5f2f492
smarkwal <stephan@markwalder.net>
2025-02-17 13:11:58 +0100

    Docs: Update example reports.

--- docs/example-reports.sh
+++ docs/example-reports.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+version=3.0.0-SNAPSHOT
+
 cd ..
-java -jar jarhc/build/libs/jarhc-3.0.0-app.jar --options docs/example-report-asm-7.0-options.txt
-java -jar jarhc/build/libs/jarhc-3.0.0-app.jar --options docs/example-report-asm-commons-7.0-options.txt
-java -jar jarhc/build/libs/jarhc-3.0.0-app.jar --options docs/example-report-asm-7.0-provided-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-asm-7.0-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-asm-commons-7.0-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-asm-7.0-provided-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-jakarta-ee-8-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-jakarta-ee-9-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-jakarta-ee-10-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-jarhc-1.7-options.txt
+java -jar jarhc/build/libs/jarhc-${version}-app.jar --options docs/example-report-jarhc-2.2.2-options.txt
