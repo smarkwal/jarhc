cbfec522c1a1cbfb37fc4e3d4d036ceed4d2a78d
smarkwal <stephan@markwalder.net>
2026-06-23 15:45:36 +0200

    Code: Change LOGGER declaration to static final

--- jarhc/src/test/java/org/jarhc/test/server/repo/RepoHandler.java
+++ jarhc/src/test/java/org/jarhc/test/server/repo/RepoHandler.java
@@ -31,7 +31,7 @@ import org.slf4j.LoggerFactory;
 
 public class RepoHandler implements HttpHandler {
 
-	private final Logger LOGGER = LoggerFactory.getLogger(RepoHandler.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(RepoHandler.class);
 
 	private static final Map<String, String> contentTypes = new HashMap<>();
 
